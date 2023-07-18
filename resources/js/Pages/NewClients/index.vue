500d79d98555911a14f7fe8f7fd4a2c19e443880
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-18 18:25:17 +0200

    fix index.vue

---
+++ resources/js/Pages/NewClients/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="New Client" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Aggiungi nuovo cliente</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <h1>Form Nuovo cliente</h1>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
