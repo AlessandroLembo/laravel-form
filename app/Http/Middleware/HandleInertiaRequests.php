66d058b6991adf0f43561337dff86c9638c2c690
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-18 22:24:00 +0200

    import model and get clients

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
+use App\Models\Client;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -33,6 +34,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
+                'clients' => Client::all()
             ],
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
