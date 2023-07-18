61a95164ee5d4f8af71217d0450ca1e72ba2fa59
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-18 18:24:34 +0200

    changes link name

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -28,10 +28,10 @@ const showingNavigationDropdown = ref(false);
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
+                                    Clients
                                 </NavLink>
                                 <NavLink :href="route('Clients.index')" :active="route().current('Clients.index')">
-                                    Clients
+                                    Add New Client
                                 </NavLink>
                             </div>
                         </div>
