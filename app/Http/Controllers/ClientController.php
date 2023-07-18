bdda96ffc51a842ba350bfa651aed9ca8cb78bbd
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-18 18:05:44 +0200

    index method

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Clients/index', [
+            'clients' => Client::all()
+        ]);
+    }
 }
