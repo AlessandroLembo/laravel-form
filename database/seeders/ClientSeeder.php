9c3ce453be2dc4a64ab2dc4e9d0d3cf33fad39bd
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-19 18:32:40 +0200

    change type data in seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -18,35 +18,35 @@ class ClientSeeder extends Seeder
                 'code' => '0023458',
                 'name' => 'CilentoMania',
                 'email' => 'cilentomania@yahoo.it',
-                'phone_number' => '3334455666',
+                'phone_number' => 3334455666,
                 'vat_number' => '12345678987'
             ],
             [
                 'code' => '0044667',
                 'name' => 'EvolutionTech',
                 'email' => 'evolution@libero.it',
-                'phone_number' => '3284422111',
+                'phone_number' => 3284422111,
                 'vat_number' => '12332112345'
             ],
             [
                 'code' => '0033888',
                 'name' => 'SummerCommunication',
                 'email' => 'summer@libero.it',
-                'phone_number' => '3236699089',
+                'phone_number' => 3236699089,
                 'vat_number' => '22233344455'
             ],
             [
                 'code' => '0055221',
                 'name' => 'BorgoBurger',
                 'email' => 'burger@yahoo.it',
-                'phone_number' => '3256677888',
+                'phone_number' => 3256677888,
                 'vat_number' => '23456789989'
             ],
             [
                 'code' => '0077331',
                 'name' => 'NewEnergy',
                 'email' => 'energy@yahoo.it',
-                'phone_number' => '3209988345',
+                'phone_number' => 3209988345,
                 'vat_number' => '33554422777'
             ],
 
