8003a6a0ba61f845a5da3ba07b03a288e75de678
AlessandroLembo <alessandrolembo2@gmail.com>
2023-07-19 12:57:03 +0200

    method store in controller to save new client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -18,5 +19,23 @@ class ClientController extends Controller
     public function create()
     {
         return Inertia::render('Clients/Create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $data = $request->all();
+
+        $new_client = new Client();
+
+        $new_client->codice = $data['code'];
+        $new_client->partita_iva = $data['vat_number'];
+        $new_client->nome = $data['name'];
+        $new_client->email = $data['email'];
+        $new_client->telefono = $data['phone_number'];
+
+        $new_client->save();
+
+        return to_route('Clients');
     }
 }
